8f87ddd0e2b04af6fb289ad1e1ccfc0d87463171
rahul14kumar <rahul.kumar.it25@heritageit.edu.in>
2024-03-16 02:23:05 +0530

    add some css in navbar

--- port/components/Navbar/navbar.js
+++ port/components/Navbar/navbar.js
@@ -7,16 +7,16 @@ import ModalBox from './ModalBox'
 function navbar() {
     const [modal,setModal]=useState(false);
   return (
-    <div>
+    <div className='pos backdrop-blur-sm'>
       <div className='bg-violet-100 p-3 flex items-center justify-between'>
         <div className='w-20 h-20 border-4 border-violet-950 rounded-full flex items-center justify-center overflow-hidden' onClick={()=>setModal(!modal)} >
             <Image src='/logo.jpeg' alt="" width={80} height={80} className='nav' />
         </div>
         
         <div className='p-2 flex overflow-hidden'>
-            <div className='mx-4 p-2 text-xl font-mono text-voilet-950 hover:border-b-4 hover:border-b-violet-950'>Home</div>
-            <div className='mx-4 p-2 text-xl font-mono text-voilet-950 hover:border-b-4 hover:border-b-violet-950'>Projects</div>
-            <div className='mx-4 p-2 text-xl font-mono text-voilet-950 hover:border-b-4 hover:border-b-violet-950'>Educations</div>
+            <div className='mx-4 p-2 text-xl font-mono text-voilet-950 hover:border-b-4 hover:border-b-violet-950 font-bold tracking-widest'>Home</div>
+            <div className='mx-4 p-2 text-xl font-mono text-voilet-950 hover:border-b-4 hover:border-b-violet-950 font-bold tracking-widest'>Projects</div>
+            <div className='mx-4 p-2 text-xl font-mono text-voilet-950 hover:border-b-4 hover:border-b-violet-950 font-bold tracking-widest'>Educations</div>
         </div>
       </div>
 
