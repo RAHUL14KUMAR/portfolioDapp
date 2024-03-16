f48f6d2073888612cdbe6ca31bffd05f646def37
rahul14kumar <rahul.kumar.it25@heritageit.edu.in>
2024-03-16 14:12:05 +0530

    home done

--- port/components/Home/Home.js
+++ port/components/Home/Home.js
@@ -5,32 +5,32 @@ import { motion } from "framer-motion";
 
 function Home() {
   return (
-    <div className='w-screen h-screen flex p-5 overflow-hidden items-center justify-evenly'>
+    <div className='w-screen h-screen flex p-3 overflow-hidden items-center justify-evenly bg-white'>
       <motion.div whileInView={{x:[0,60]}}
     transition={{duration:0.5, type:"tween"}} className='mt-[6em] img'>
-        <Image src='/home.gif' alt="/logo.jpeg" width={54} height={44} className='nav'/>
+        <Image src='/home.gif' alt="/logo.jpeg" width={54} height={44} className='nav -Z-1'/>
       </motion.div>
 
       <motion.div whileInView={{x:[60,0]}}
-    transition={{duration:0.5, type:"tween"}}className='mt-[8em] nxt bg-white text-right p-5'>
-        <div className='text-cyan-950 font-mono font-bold tracking-widest text-2xl p-5'>
+    transition={{duration:0.5, type:"tween"}}className='mt-[8em]  text-right p-3'>
+        <div className='text-violet-950 font-mono font-bold tracking-widest text-2xl p-5'>
             <h1>Hi,I am <span className='text-3xl underline'>Rahul Kumar</span></h1>
 
-            <div className='mt-3 text-cyan-950 font-sans font-bold text-2xl'>  3rd year student</div>
+            <div className='mt-3 text-violet-950 font-sans font-bold text-2xl'>  3rd year student</div>
 
-            <div className='mt-3 text-cyan-950 font-sans font-bold text-2xl'>A FULL STAK DEVELOPER</div>
+            <div className='mt-3 text-violet-950 font-sans font-bold text-2xl'>A FULL STACK DEVELOPER</div>
 
-            <div className='mt-3 text-cyan-950 font-sans font-bold text-2xl'>  Enter in the field of Blockchain</div>
+            <div className='mt-3 text-violet-950 font-sans font-bold text-2xl'>  Enter in the field of Blockchain</div>
             
-            <div className='mt-3 text-cyan-950 font-sans font-bold text-2xl'>Eager to learn new Tech</div>
+            <div className='mt-3 text-violet-950 font-sans font-bold text-2xl'>Eager to learn new Tech</div>
 
             <div className='flex items-center
             justify-evenly'>
-                <div className='mt-[4em] bg-violet-300 p-5 rounded-lg text-violet-950 font-mono font-bold text-center text-sm'>
+                <div className='mt-[4em] bg-violet-300 p-5 rounded-lg text-violet-950 font-mono font-bold text-right text-sm'>
                     Backend developer
                 </div>
 
-                <div className='mt-[4em] bg-cyan-300 p-5 rounded-lg text-cyan-950 font-mono font-bold text-center text-sm'>
+                <div className='mt-[4em] bg-cyan-300 p-5 rounded-lg text-cyan-950 font-mono font-bold text-right text-sm'>
                     web3 developer
                 </div>
             </div>
